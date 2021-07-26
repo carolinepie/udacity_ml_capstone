c092cf413c6388f21785415d9d67c423d889eb24
EC2 Default User <ec2-user@ip-172-16-77-187.ec2.internal>
2021-07-26 01:56:35 +0000

    adding final copy

--- lstm/model.py
+++ lstm/model.py
@@ -17,25 +17,26 @@ class LSTMClassifier(nn.Module):
         start, end = n//4, n//2
         self.lstm.bias_hh_l0.data[start:end].fill_(1.)
         
-        interim = 1
+        interim = 12
         self.dense = nn.Linear(in_features=hidden_dim, out_features=interim)
         kaiming_normal_(self.dense.weight.data)
         self.activation = nn.ReLU()
         
         self.dense2 = nn.Linear(in_features=interim, out_features=1)
         kaiming_normal_(self.dense2.weight.data)
-#         self.activation2 = nn.ReLU()
+        self.activation2 = nn.ReLU()
     def forward(self, x):
         """
         Perform a forward pass of our model on some input.
         """
         lstm_out, (h_n, c_n) = self.lstm(x)
         out = self.dense(lstm_out[:, -1])
-#         linear1 = self.activation(out)
+        linear1 = self.activation(out)
         
-#         out2 = self.dense2(linear1)
-#         final = self.activation2(out2)        
+        out2 = self.dense2(linear1)
+#         final = self.activation2(out2)    
+        final = out2
         
 #         print('final')
 #         print(final)
-        return out
+        return final
